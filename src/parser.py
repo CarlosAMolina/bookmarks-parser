2cf26d1b4966d867baba26b25047f6aa0c0cba85
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-08-21 22:38:06 +0200

    add bs4

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,20 @@
+from bs4 import BeautifulSoup
+
+
 def parse_file(path_name: str):
     print(f"Start parsing {path_name}")
+    _Parser().run(path_name)
+
+
+class _Parser:
+    def run(self, path_name: str):
+        with open(path_name, "r") as f:
+            soup = BeautifulSoup(f, "html.parser")
+            self._process_beautiful_soup(soup)
+
+    def _process_beautiful_soup(self, soup: BeautifulSoup):
+        print(soup.prettify())
+        print(type(soup))
 
 
 # TODO rm
