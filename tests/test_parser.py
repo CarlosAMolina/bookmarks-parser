0b8474113350609db5a1f5cf069d60e36c1a9718
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-08-22 22:41:03 +0200

    change test values

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -11,9 +11,9 @@ class TestFileParser(unittest.TestCase):
         file_path_name = str(file_path)
         expected_results = [
             "[20240822 prices](http://invented-prices-url.foo/)",
-            "[20240822 restaurants](http://invented-restaurants-url.foo/)",
+            "[20230511 restaurants](http://invented-restaurants-url.foo/)",
             "[20240822 photos-2020](http://invented-photos-2020-url.foo/)",
-            "[20240822 photos-2022](http://invented-photos-2022-url.foo/)",
+            "[20200507 photos-2022](http://invented-photos-2022-url.foo/)",
             "[20240822 bus hours](https://invented-bus-hours.foo/)",
         ]
         result = [str(parsed_url) for parsed_url in parser._FileParser().get_urls(file_path_name)]
