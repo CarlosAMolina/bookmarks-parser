27c588d8742daec46290c22d806ffe2f34ed8b3a
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-08-22 21:50:05 +0200

    add _Url class

--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,4 @@
-import bs4
+import datetime
 from bs4 import BeautifulSoup
 
 
@@ -11,6 +11,31 @@ class _Parser:
     def run(self, path_name: str):
         with open(path_name, "r") as f:
             soup = BeautifulSoup(f, "html.parser")
-        urls = soup.find_all('a')
+        urls = soup.find_all("a")
         for url in urls:
-            print(url)
+            return _Url(
+                url["add_date"],
+                url["href"],
+                url.text,
+            )
+
+
+class _Url:
+    def __init__(
+        self,
+        creation_date_epoch_str: str,
+        href: str,
+        name: str,
+    ):
+        self._creation_date_epoch_str = creation_date_epoch_str
+        self.href = href
+        self.name = name
+
+    @property
+    def creation_date_str(self) -> str:
+        creation_date_epoch = int(self._creation_date_epoch_str)
+        creation_date = self._get_datetime_from_epoch_str(creation_date_epoch)
+        return creation_date.strftime("%Y%m%d")
+
+    def _get_datetime_from_epoch_str(self, epoch: int) -> datetime.date:
+        return datetime.datetime.fromtimestamp(epoch)
